88a19c32fa065761a9f3841c3ded5777c70acde4
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-10-12 06:18:59 +0530

    Add files via upload
    Reversing a Single Linked List in C

---
+++ Reversing _SLL.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* link;
+};
+struct node* reverse(struct node* head)
+{
+    struct node* prev=NULL;
+    struct node* next = NULL;
+
+    while(head!=NULL)
+    {
+        next=head->link;
+        head->link=prev;
+        prev=head;
+        head=next;
+    }
+    head=prev;
+    return head;
+}
+int main()
+{
+    struct node *head, *ptr,*newnode;
+    head=0;
+    int n=0;
+    while(n<5)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("Enter an integer = ");
+        scanf("%d",&newnode->data);
+
+        newnode->link=0;
+
+        if(head==0)
+        {
+            head=ptr=newnode;
+
+        }
+        else
+        {
+            ptr->link=newnode;
+            ptr=newnode;
+
+        }
+        n++;
+     }
+
+    head=reverse(head);
+
+    ptr=head;
+    printf("\n");
+    while(ptr!=NULL)
+    {
+        printf("%d\t",ptr->data);
+        ptr=ptr->link;
+    }
+
+    return 0;
+}
+
+
